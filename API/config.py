87082fb247cd511bab385d8a65d0a9e7797581c0
Adesola Busari <adesolaisa3@gmail.com>
2024-10-23 09:36:01 +0100

    updated database

--- API/config.py
+++ API/config.py
@@ -4,6 +4,7 @@ from decouple import config
 
 
 BASE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
 class Config:
     SECRET_kEY = config("SECRET_KEY",'secret')
     SQLALCHEMY_TRACKMODIFICATIONS =False
@@ -21,7 +22,11 @@ class TestConfig(Config):
 
 
 class Prod(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # postgresql://database_8ss6_user:42Vv2cC3Zmi6trpaMNTTqx4wL3umJBF8@dpg-csc7ol88fa8c73fqqmag-a.oregon-postgres.render.com/database_8ss6
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG=config('DEBUG', cast=bool)
+BASE_DIRECTORY
 
 
 config_dict ={
---
+++ API/wsgi.py
@@ -0,0 +1,7 @@
+# from API import create_app
+# from flask import Flask
+# app= Flask(__name__)
+
+# if __name__ == "__main__":
+#     import waitress
+#     waitress.serve(app,)
