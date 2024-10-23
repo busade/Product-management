0e1670177bf231cfaf783dda3a158af84ada1777
Adesola Busari <adesolaisa3@gmail.com>
2024-10-23 10:04:09 +0100

    new commit

--- API/config.py
+++ API/config.py
@@ -6,7 +6,7 @@ from decouple import config
 BASE_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 class Config:
-    SECRET_kEY = config("SECRET_KEY",'secret')
+    SECRET_KEY = config("SECRET_KEY",'secret')
     SQLALCHEMY_TRACKMODIFICATIONS =False
 
 class DevConfig(Config):
@@ -21,16 +21,15 @@ class TestConfig(Config):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-class Prod(Config):
+class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # postgresql://database_8ss6_user:42Vv2cC3Zmi6trpaMNTTqx4wL3umJBF8@dpg-csc7ol88fa8c73fqqmag-a.oregon-postgres.render.com/database_8ss6
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG=config('DEBUG', cast=bool)
+    DEBUG=config('DEBUG', default = False, cast=bool)
 BASE_DIRECTORY
 
 
 config_dict ={
     'dev':DevConfig,
     'test':TestConfig,
-    'prod':Prod
+    'prod':ProdConfig
 }
